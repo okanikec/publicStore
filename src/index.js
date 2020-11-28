b94c7adf4445b4ec6222df5960f1bfcdb89cbb57
okanikec <okanikec@isu.edu>
2020-11-28 09:53:36 -0600

    init cmmit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require ('express')
+const http = require('http')
+const path = require('path')
+const socketio = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+
+
+
+app.use(express.static(path.join(__dirname, '../public')))
+
+
+
+io.on('connection', () => {
+    console.log('New websocket connection')
+})
+
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}!`)
+})
